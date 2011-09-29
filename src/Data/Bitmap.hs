f3f8c8b955822cb569d76b7cdbbd978e769f2aa1
Jason Dagit <dagitj@gmail.com>
2011-09-29 07:22:07 -0700

    Produce bmp files more directly instead of PPM lazily.

---
+++ src/Data/Bitmap.hs
@@ -0,0 +1,49 @@
+module Data.Bitmap
+( BMP
+, allocBMP 
+, pokePixel
+, writeBMP
+) where
+
+import qualified Codec.BMP as BMP
+import qualified Data.ByteString.Internal as BI
+import Data.Word (Word8)
+import Foreign.ForeignPtr
+import Foreign.Storable
+
+type RGB = (Word8, Word8, Word8)
+
+rgbaSize :: Int
+rgbaSize = 4
+
+data BMP = BMP
+  { bmpChunk  :: !(ForeignPtr Word8)
+  , bmpWidth  :: !Int
+  , bmpHeight :: !Int }
+
+allocBMP :: Int -> Int -> IO BMP
+allocBMP width height = do
+  mem <- mallocForeignPtrBytes sz
+  return $! BMP mem width height
+  where sz = rgbaSize * width * height
+
+pokePixel :: Int -> Int -> RGB -> BMP -> IO ()
+pokePixel x y _ (BMP chnk w h)
+  | x < 0 || x > w || y < 0 || y > h = error "pokePixel: out of bounds"
+pokePixel x y rgb bmp = unsafePokePixel x y rgb bmp
+
+unsafePokePixel :: Int -> Int -> RGB -> BMP -> IO ()
+unsafePokePixel x y (r,g,b) (BMP mem w _) = do
+  let off = x*rgbaSize + y*w*rgbaSize
+  withForeignPtr mem $ \ptr -> do
+    pokeElemOff ptr off r
+    pokeElemOff ptr (off+1) g
+    pokeElemOff ptr (off+2) b
+    pokeElemOff ptr (off+3) 0
+
+bmpToBMP :: BMP -> BMP.BMP
+bmpToBMP (BMP rgba w h) = BMP.packRGBA32ToBMP w h bs
+  where bs = BI.fromForeignPtr rgba 0 (w*h*rgbaSize)
+
+writeBMP :: FilePath -> BMP -> IO ()
+writeBMP fp bmp = BMP.writeBMP fp (bmpToBMP bmp)
--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,8 @@ import Data.RGB
 import Data.Shape
 import Data.HitRecord
 import Data.VectorSpace
+import Data.Bitmap
+import Control.Monad ( forM_ )
 
 main :: IO ()
 main = do
@@ -19,37 +21,27 @@ main = do
                                           (Vec3 0 100 (-1000))
                                           (Vec3 450 20 (-1000))
                                           (Vec3 0.8 0.2 0.2)) ]
-      hits = 
-        for [499, 498 .. 0] $ \j ->
-          for [0 .. 499] $ \i ->
-            let tmax = 100000
-                r = Ray (Vec3 i j 0) dir
-                keepClosest :: Ord a => [Maybe (HitRecord a)]
-                            -> Maybe (HitRecord a)
-                keepClosest [Nothing, Nothing] = Nothing
-                keepClosest [Nothing, h] = h
-                keepClosest [h, Nothing] = h
-                keepClosest [Just h1, Just h2] | (hrT h1) <= (hrT h2) = Just h1
-                                               | otherwise = Just h2
-                hit = keepClosest $ for shapes $ \shape ->
-                  case shapeHit shape r 0.00001 tmax 0 of
-                    Just rec -> Just rec
-                    Nothing -> Nothing
-            in hit
-  putStrLn "P3"
-  putStrLn "500 500"
-  putStrLn "255"
-  mapM_ (\hrs -> ppmOut hrs >> putStrLn "") hits
-
-ppmOut :: (Ord a, Enum a, Floating a) => [Maybe (HitRecord a)] -> IO ()
-ppmOut [] = return ()
-ppmOut (Just hr:hrs) = do
-  putStr $ show (toWord8 (clamp (getR (hrColor hr)))) ++ " "
-  putStr $ show (toWord8 (clamp (getG (hrColor hr)))) ++ " "
-  putStr $ show (toWord8 (clamp (getB (hrColor hr)))) ++ " "
-  ppmOut hrs
-ppmOut (Nothing : hrs) = do
-  putStr $ show (toWord8 0.2) ++ " "
-  putStr $ show (toWord8 0.2) ++ " "
-  putStr $ show (toWord8 0.2) ++ " "
-  ppmOut hrs
+  bmp <- allocBMP 500 500 
+  forM_ [499, 498 .. 0] $ \j ->
+    forM_ [0 .. 499] $ \i -> do
+      let tmax = 100000
+          r = Ray (Vec3 (fromIntegral i) (fromIntegral j) 0) dir
+          keepClosest :: Ord a => [Maybe (HitRecord a)]
+                      -> Maybe (HitRecord a)
+          keepClosest [Nothing, Nothing] = Nothing
+          keepClosest [Nothing, h] = h
+          keepClosest [h, Nothing] = h
+          keepClosest [Just h1, Just h2] | (hrT h1) <= (hrT h2) = Just h1
+                                         | otherwise = Just h2
+          hit = keepClosest $ for shapes $ \shape ->
+            case shapeHit shape r 0.00001 tmax 0 of
+              Just rec -> Just rec
+              Nothing -> Nothing
+      case hit of
+        Just hr ->
+          pokePixel i j ((toWord8 (clamp (getR (hrColor hr))))
+                        ,(toWord8 (clamp (getG (hrColor hr))))
+                        ,(toWord8 (clamp (getB (hrColor hr))))) bmp
+        Nothing ->
+          pokePixel i j ((toWord8 0.2), (toWord8 0.2), (toWord8 0.2)) bmp
+  writeBMP "raytracer.bmp" bmp
