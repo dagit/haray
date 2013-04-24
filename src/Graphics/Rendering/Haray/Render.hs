64263b0e8732bcd00786d4bcbdee08c191b29201
Jason Dagit <dagit@galois.com>
2013-04-23 19:10:01 -0700

    Refactoring rendering a tiny bit to add multisampling based on a random distribution

--- src/Graphics/Rendering/Haray/Render.hs
+++ src/Graphics/Rendering/Haray/Render.hs
@@ -12,9 +12,10 @@ import Graphics.Rendering.Haray.HitRecord
 import Graphics.Rendering.Haray.Luminaire
 import Graphics.Rendering.Haray.Bitmap
 import Numeric.LinearAlgebra.Vector
-import Control.Monad ( forM_, when )
+import Control.Monad ( forM_, forM, when )
 import Control.Monad.ST ( stToIO, RealWorld )
 import System.IO ( hFlush, stdout )
+import System.Random
 import Codec.Picture.Types ( MutableImage(..), PixelRGB8(..) )
 
 renderSceneFromTo :: FilePath -> FilePath -> IO ()
@@ -30,6 +31,9 @@ renderSceneFromFile from = do
   putStr $ "Rendering:"
   renderScene scene
 
+for :: [a] -> (a -> b) -> [b]
+for = flip map
+
 renderScene :: [SceneElement] -> IO (MutableImage RealWorld PixelRGB8)
 renderScene scene = do
   shapes <- mkShapes scene
@@ -37,7 +41,6 @@ renderScene scene = do
       c'               = S.mkCamera scene
       directedLights   = mkDirectedLights scene
       ambientLight     = maybe defaultAmbient id (mkAmbientLight scene)
-      for              = flip map
       comp x y         = hrT x `compare` hrT y
       -- TODO: make these defaults configurable
       defaultCamera    = (C.mkCamera (Vec3 0.0 0.0 0.0)
@@ -49,43 +52,52 @@ renderScene scene = do
   img <- stToIO $ mkImage nx ny
   forM_ [0 .. (ny-1)] $ \j ->
     forM_ [0 .. (nx-1)] $ \i -> do
-      let tmax = 100000
-          r = getRay camera (((fromIntegral i)+0.5)/fromIntegral nx)
-                            (((fromIntegral j)+0.5)/fromIntegral ny)
-          hit = listToMaybe $ sortBy comp $ catMaybes $
-            for shapes $ \shape -> shapeHit shape r 0.00001 tmax 0
-          progress = i+j*nx
+      hs <- forM [1..4::Int] $ \_ -> do -- 4 samples per pixel
+        ry <- randomRIO (-0.4,0.4)
+        rx <- randomRIO (-0.4,0.4)
+        let tmax = 100000
+            r = getRay camera (((fromIntegral i)+rx+0.5)/fromIntegral nx)
+                              (((fromIntegral j)+ry+0.5)/fromIntegral ny)
+            hit = listToMaybe $ sortBy comp $ catMaybes $
+              for shapes $ \shape -> shapeHit shape r 0.00001 tmax 0
+        return (r,hit)
+      let progress = i+j*nx
           tenpercent = fromIntegral nx * fromIntegral ny * (0.1::RealTy)
       when ((progress `mod` (round tenpercent)) == 0) (putStr ".")
       hFlush stdout
-      case hit of
-        Just hr -> do
-          let
-              ca       = alColor ambientLight
-              cr       = hrHitTex hr (Vec2 0 0) {- TODO: implement UV -} at
-              cp       = Vec3 1 1 1 <-> cr
-              n        = hrNormal hr
-              e        = unitVector $ vNegate $ rayDirection r
-              p        = 32
-              at       = rayOrigin r <+> (hrT hr*> rayDirection r)
-              vsum     = foldl' (<+>) (Vec3 0 0 0)
-              c        = (cr <*> (ca <+> vsum diffs)) <+> vsum phongs
-              (diffs, phongs) = unzip lightingTerms
-              lightingTerms   = for directedLights $ \dl ->
-                let  cl       = dlColor dl
-                     l        = unitVector $ vNegate $ dlDirection dl
-                     h        = unitVector $ e <+> l
-                     hn       = h<.>n
-                     m        = max 0 (n <.> l)
-                     inShadow = or $ for shapes $ \shape ->
-                       shapeShadowHit shape (Ray at l) 0.01 tmax 0
-                     sc = if inShadow
-                           then (Vec3 0 0 0, Vec3 0 0 0)
-                           else (m*>cl, (hn**p)*>(cl<*>cp))
-                in sc
-          writePixelRGBIO img i j (PixelRGB8 (toWord8 (clamp (getR c)))
-                                             (toWord8 (clamp (getG c)))
-                                             (toWord8 (clamp (getB c))))
-        Nothing ->
-          writePixelRGBIO img i j (PixelRGB8 (toWord8 (0.2::RealTy)) (toWord8 (0.2::RealTy)) (toWord8 (0.2::RealTy)))
+      let cs   = map (processHit shapes directedLights ambientLight) hs
+          avgC = foldl1' (<+>) cs </ genericLength cs
+      writePixelRGBIO img i j (PixelRGB8 (toWord8 (clamp (getR avgC)))
+                                         (toWord8 (clamp (getG avgC)))
+                                         (toWord8 (clamp (getB avgC))))
   return img 
+
+processHit :: [Shape RealTy] -> [DirectedLight RealTy] -> AmbientLight RealTy -> (Ray RealTy, Maybe (HitRecord RealTy)) -> RGB RealTy
+processHit shapes directedLights ambientLight (r,hit) = case hit of
+  Just hr ->
+    let
+        ca       = alColor ambientLight
+        cr       = hrHitTex hr (Vec2 0 0) {- TODO: implement UV -} at
+        cp       = Vec3 1 1 1 <-> cr
+        n        = hrNormal hr
+        e        = unitVector $ vNegate $ rayDirection r
+        p        = 32
+        at       = rayOrigin r <+> (hrT hr*> rayDirection r)
+        vsum     = foldl' (<+>) (Vec3 0 0 0)
+        c        = (cr <*> (ca <+> vsum diffs)) <+> vsum phongs
+        tmax     = 100000 -- TODO: hack refactor me
+        (diffs, phongs) = unzip lightingTerms
+        lightingTerms   = for directedLights $ \dl ->
+          let  cl       = dlColor dl
+               l        = unitVector $ vNegate $ dlDirection dl
+               h        = unitVector $ e <+> l
+               hn       = h<.>n
+               m        = max 0 (n <.> l)
+               inShadow = or $ for shapes $ \shape ->
+                 shapeShadowHit shape (Ray at l) 0.01 tmax 0
+               sc = if inShadow
+                     then (Vec3 0 0 0, Vec3 0 0 0)
+                     else (m*>cl, (hn**p)*>(cl<*>cp))
+          in sc
+    in c
+  Nothing -> Vec3 0.2 0.2 0.2
